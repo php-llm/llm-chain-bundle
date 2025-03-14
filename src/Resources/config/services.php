399984afede37a37598e28c5d2067619fad7c0ad
Christopher Hertel <mail@christopher-hertel.de>
2025-03-15 00:36:04 +0100

    chore: 0.19 compatibility

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -7,26 +7,18 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use PhpLlm\LlmChain\Chain\StructuredOutput\ChainProcessor as StructureOutputProcessor;
 use PhpLlm\LlmChain\Chain\StructuredOutput\ResponseFormatFactory;
 use PhpLlm\LlmChain\Chain\StructuredOutput\ResponseFormatFactoryInterface;
-use PhpLlm\LlmChain\Chain\ToolBox\ChainProcessor as ToolProcessor;
-use PhpLlm\LlmChain\Chain\ToolBox\MetadataFactory;
-use PhpLlm\LlmChain\Chain\ToolBox\MetadataFactory\ReflectionFactory;
-use PhpLlm\LlmChain\Chain\ToolBox\ToolBox;
-use PhpLlm\LlmChain\Chain\ToolBox\ToolBoxInterface;
-use PhpLlm\LlmChain\Embedder;
+use PhpLlm\LlmChain\Chain\Toolbox\ChainProcessor as ToolProcessor;
+use PhpLlm\LlmChain\Chain\Toolbox\MetadataFactory;
+use PhpLlm\LlmChain\Chain\Toolbox\MetadataFactory\ReflectionFactory;
+use PhpLlm\LlmChain\Chain\Toolbox\Toolbox;
+use PhpLlm\LlmChain\Chain\Toolbox\ToolboxInterface;
 use PhpLlm\LlmChainBundle\Profiler\DataCollector;
-use PhpLlm\LlmChainBundle\Profiler\TraceableToolBox;
+use PhpLlm\LlmChainBundle\Profiler\TraceableToolbox;
 
 return static function (ContainerConfigurator $container): void {
     $container->services()
         ->defaults()
             ->autowire()
-
-        // high level feature
-        ->set('llm_chain.embedder.abstract', Embedder::class)
-            ->abstract()
-            ->args([
-                '$embeddings' => abstract_arg('Embeddings model'),
-            ])
 
         // structured output
         ->set(ResponseFormatFactory::class)
@@ -37,40 +29,40 @@ return static function (ContainerConfigurator $container): void {
 
         // tools
         ->set('llm_chain.toolbox.abstract')
-            ->class(ToolBox::class)
+            ->class(Toolbox::class)
             ->autowire()
             ->abstract()
             ->args([
                 '$tools' => abstract_arg('Collection of tools'),
             ])
-        ->set(ToolBox::class)
+        ->set(Toolbox::class)
             ->parent('llm_chain.toolbox.abstract')
             ->args([
                 '$tools' => tagged_iterator('llm_chain.tool'),
             ])
-            ->alias(ToolBoxInterface::class, ToolBox::class)
+            ->alias(ToolboxInterface::class, Toolbox::class)
         ->set(ReflectionFactory::class)
             ->alias(MetadataFactory::class, ReflectionFactory::class)
         ->set('llm_chain.tool.chain_processor.abstract')
             ->class(ToolProcessor::class)
             ->abstract()
             ->args([
-                '$toolBox' => abstract_arg('Tool box'),
+                '$toolbox' => abstract_arg('Toolbox'),
             ])
         ->set(ToolProcessor::class)
             ->parent('llm_chain.tool.chain_processor.abstract')
             ->tag('llm_chain.chain.input_processor')
             ->tag('llm_chain.chain.output_processor')
             ->args([
-                '$toolBox' => service(ToolBoxInterface::class),
+                '$toolbox' => service(ToolboxInterface::class),
                 '$eventDispatcher' => service('event_dispatcher')->nullOnInvalid(),
             ])
 
         // profiler
         ->set(DataCollector::class)
             ->tag('data_collector')
-        ->set(TraceableToolBox::class)
-            ->decorate(ToolBoxInterface::class)
+        ->set(TraceableToolbox::class)
+            ->decorate(ToolboxInterface::class)
             ->tag('llm_chain.traceable_toolbox')
     ;
 };
